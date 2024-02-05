6d7d7877bed541a3548e099ebec448fb3976da41
Alex C <alex.c@euka.edu.au>
2024-02-05 22:06:57 +1100

    Improved readability of Game files

--- starter_code/Game.h
+++ starter_code/Game.h
@@ -6,6 +6,8 @@
 #ifndef APT2023_A2_GAME_H
 #define APT2023_A2_GAME_H
 
+#include <vector>
+#include <string>
 #include "LinkedList.h"
 #include "Player.h"
 #include "Board.h"
@@ -17,8 +19,13 @@ public:
     ~Game();
     void launchGame();
     bool checkForEmptyPlayerHands(Player **players, int playerCount);
-
-
+    void displayGameStatus();
+    int getPlayerMenuSelection();
+    bool processPlayerAction(int menuChoice);
+    void placeTiles();
+    void replaceTile();
+    void saveGame();
+    bool isGameComplete();
 
 private:
     Player *player1;
@@ -27,6 +34,11 @@ private:
     Board *board;
     Player *currentPlayer;
     int playerCount;
+    std::vector<std::string> parsePlayerInput();
+    bool validateTilePlacement(std::vector<std::string>& words, std::vector<Tile*>& tilesToPlace, std::vector<std::pair<int, int>>& tilePositions);
+    void updateGameAfterTilePlacement(std::vector<Tile*>& tilesToPlace, std::vector<std::pair<int, int>>& tilePositions, int& numTiles);
+    Tile* createAndValidateTile(const std::string& tileStr); // New method declaration
+    void replaceTileAndUpdateHand(Tile* tileToReplace); // New method declaration
 };
 
 #endif // APT2023_A2_GAME_H

c8da6a1eb48a383684af48126cf381239f410db4
Morgs61 <S3858530@student.rmit.edu.au>
2024-02-16 15:16:13 +1100

    board -> unicode

--- starter_code/Tile.cpp
+++ starter_code/Tile.cpp
@@ -6,14 +6,17 @@
 // Constructor implementation
 Tile::Tile(Colour colour, Shape shape) : colour(colour), shape(shape) {}
 
-bool Tile::isValidTile(Colour colour, Shape shape) {
+bool Tile::isValidTile(Colour colour, Shape shape)
+{
   // Define valid colours and shapes directly in the function
   Colour validColours[] = {RED, ORANGE, YELLOW, GREEN, BLUE, PURPLE};
   Shape validShapes[] = {CIRCLE, STAR_4, DIAMOND, SQUARE, STAR_6, CLOVER};
 
   // Check if the colour and shape are valid
-  for (size_t i = 0; i < sizeof(validColours) / sizeof(validColours[0]); ++i) {
-    if (colour == validColours[i] && shape == validShapes[i]) {
+  for (size_t i = 0; i < sizeof(validColours) / sizeof(validColours[0]); ++i)
+  {
+    if (colour == validColours[i] && shape == validShapes[i])
+    {
       return true;
     }
   }
@@ -24,11 +27,86 @@ Colour Tile::getColour() const { return colour; }
 
 Shape Tile::getShape() const { return shape; }
 // Destructor implementation (if needed)
-Tile::~Tile() {
+Tile::~Tile()
+{
   // Any destruction if needed
 }
 
-std::string Tile::toString() {
+std::string Tile::toString()
+{
   std::string retval = colour + std::to_string(shape);
+  return retval;
+}
+
+std::string Tile::toUnicode()
+{
+  std::string retval;
+
+  std::string shapeUnicode;
+
+  // Determine the Unicode symbol corresponding to the shape
+  if (shape == CIRCLE)
+  {
+    shapeUnicode = CIRCLE_UNICODE;
+  }
+  else if (shape == STAR_4)
+  {
+    shapeUnicode = FOUR_STAR_UNICODE;
+  }
+  else if (shape == DIAMOND)
+  {
+    shapeUnicode = DIAMOND_UNICODE;
+  }
+  else if (shape == SQUARE)
+  {
+    shapeUnicode = SQUARE_UNICODE;
+  }
+  else if (shape == STAR_6)
+  {
+    shapeUnicode = SIX_STAR_UNICODE;
+  }
+  else if (shape == CLOVER)
+  {
+    shapeUnicode = CLOVER_UNICODE;
+  }
+  else
+  {
+    shapeUnicode = "";
+  }
+
+  // Determine the ANSI escape code corresponding to the color
+  std::string colorCode;
+  if (colour == 'R')
+  {
+    colorCode = RED_COLOR;
+  }
+  else if (colour == 'O')
+  {
+    colorCode = ORANGE_COLOR;
+  }
+  else if (colour == 'Y')
+  {
+    colorCode = YELLOW_COLOR;
+  }
+  else if (colour == 'G')
+  {
+    colorCode = GREEN_COLOR;
+  }
+  else if (colour == 'B')
+  {
+    colorCode = BLUE_COLOR;
+  }
+  else if (colour == 'P')
+  {
+    colorCode = PURPLE_COLOR;
+  }
+  else
+  {
+    colorCode = "";
+  }
+
+  // Construct the string representation of the tile
+  retval = colorCode + shapeUnicode + RESET_COLOR + " ";
+
   return retval;
 }
--- starter_code/Tile.h
+++ starter_code/Tile.h
@@ -8,20 +8,20 @@
 // Color and Unicode escape sequences for printing colored text and Unicode
 // symbols in the console
 
-// #define RESET_COLOR "\033[0m"           // Reset color to default
-// #define RED_COLOR "\033[31m"            // Red color
-// #define ORANGE_COLOR "\u001b[38;5;209m" // Orange color
-// #define YELLOW_COLOR "\033[33m"         // Yellow color
-// #define GREEN_COLOR "\033[32m"          // Green color
-// #define BLUE_COLOR "\033[34m"           // Blue color
-// #define PURPLE_COLOR "\033[35m"         // Purple color
+#define RESET_COLOR "\033[0m"           // Reset color to default
+#define RED_COLOR "\033[31m"            // Red color
+#define ORANGE_COLOR "\u001b[38;5;209m" // Orange color
+#define YELLOW_COLOR "\033[33m"         // Yellow color
+#define GREEN_COLOR "\033[32m"          // Green color
+#define BLUE_COLOR "\033[34m"           // Blue color
+#define PURPLE_COLOR "\033[35m"         // Purple color
 
-// #define CIRCLE_UNICODE "\U000025EF"    // Circle symbol
-// #define FOUR_STAR_UNICODE "\U00002726" // Four star symbol
-// #define DIAMOND_UNICODE "\U000025C6"   // Diamond symbol
-// #define SQUARE_UNICODE "\U000025FC"    // Square symbol
-// #define SIX_STAR_UNICODE "\U0001F7CC"  // Six star symbol
-// #define CLOVER_UNICODE "\U00002618"    // Clover symbol
+#define CIRCLE_UNICODE "\U000025EF"    // Circle symbol
+#define FOUR_STAR_UNICODE "\U00002726" // Four star symbol
+#define DIAMOND_UNICODE "\U000025C6"   // Diamond symbol
+#define SQUARE_UNICODE "\U000025FC"    // Square symbol
+#define SIX_STAR_UNICODE "\U0001F7CC"  // Six star symbol
+#define CLOVER_UNICODE "\U00002618"    // Clover symbol
 
 // Define a Colour type
 typedef char Colour;
@@ -29,8 +29,9 @@ typedef char Colour;
 // Define a Shape type
 typedef int Shape;
 
-class Tile {
- public:
+class Tile
+{
+public:
   Tile(Colour colour, Shape shape);
   ~Tile();
 
@@ -40,11 +41,14 @@ class Tile {
   // Add toString function declaration
   std::string toString();
 
+  // Add toUnicode function declaration
+  std::string toUnicode();
+
   bool isValidTile(Colour colour, Shape shape);
 
- private:
+private:
   Colour colour;
   Shape shape;
 };
 
-#endif  // ASSIGN2_TILE_H
+#endif // ASSIGN2_TILE_H
